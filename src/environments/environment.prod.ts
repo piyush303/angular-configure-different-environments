349b1c46ec601494b19bf9cba64b4b4690ebb221
Piyush Jain <piyjain4@WKMZT843B142.global.publicisgroupe.net>
2022-02-28 18:18:57 +0530

    configured environments for qa and uat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  envName: 'PROD',
+  apiUrl: 'https://prod.endpoint.com'
 };
---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,5 @@
+export const environment = {
+	production: true,
+	envName: 'UAT',
+	apiUrl: 'https://uat.endpoint.com'
+};
